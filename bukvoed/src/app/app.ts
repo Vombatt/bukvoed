65833d1b12c0c52664dad3a548644e03e46ab2f4
Vombatt <gon4arovlex@gmail.com>
2026-08-16 13:26:00 +0200

    Add WordService demonstration to App component

--- bukvoed/src/app/app.ts
+++ bukvoed/src/app/app.ts
@@ -1,17 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { environment } from '../environments/environment';
-
+import { WordService } from './services/word.service';
 @Component({
   selector: 'app-root',
   imports: [MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('bukvoed');
+  private wordService = inject(WordService);
 
   constructor() {
     console.log('Pixabay API Key:', environment.pixabayApiKey);
+  }
+
+  async ngOnInit() {
+    await this.wordService.loadWords();
+    const word = this.wordService.getRandomWord(4);
+    if (word) {
+      const letters = this.wordService.generateLetterSet(word);
+      console.log(`Выбранное слово: ${word}`);
+      console.log(`Сгенерированный набор букв: ${letters.join(', ')}`);
+    }
   }
 }
